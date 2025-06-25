80480497652a70b97ccf2b55b79dc89c14bd5b64
allan <allanvicthor123456789@gmail.com>
2025-06-24 21:11:17 -0300

    ok

--- code/Const.py
+++ code/Const.py
@@ -6,6 +6,24 @@ COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 0)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Player1': 3,
+    'Player1Shot': 1,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
+}
+
+
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
     'Level1Bg1': 999,
@@ -19,22 +37,6 @@ ENTITY_HEALTH = {
     'Enemy1': 50,
     'Enemy2': 60,
 }
-
-ENTITY_SPEED = {
-    'Level1Bg0': 0,
-    'Level1Bg1': 1,
-    'Level1Bg2': 2,
-    'Level1Bg3': 3,
-    'Level1Bg4': 4,
-    'Level1Bg5': 5,
-    'Level1Bg6': 6,
-    'Player1': 3,
-    'Player2': 3,
-    'Enemy1': 2,
-    'Enemy2': 1,
-}
-
-EVENT_ENEMY = pygame.USEREVENT + 1
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
