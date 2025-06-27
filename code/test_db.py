3128b8843522ec43a27bc35c3ce9cfa9a0714a7a
allan <allanvicthor123456789@gmail.com>
2025-06-27 20:41:39 -0300

    ok

---
+++ code/test_db.py
@@ -0,0 +1,12 @@
+
+
+# start_y = SCORE_POS['Label'][1] + 30  # Começa abaixo do cabeçalho
+#     for i, player_score in enumerate(list_score):
+#         id_, name, score, date = player_score
+#         pos_y = start_y + i * 30
+#         self.score_text(
+#             20,
+#             f'{name:<8} {score:05d}    {date}',
+#             C_YELLOW,
+#             (SCORE_POS['Title'][0], pos_y)  # mesmo X centralizado que o título
+#         )
